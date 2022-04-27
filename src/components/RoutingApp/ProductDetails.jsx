2978714821106677d64a77eb07bfdfcbc7a889d1
Code With Z <codewithz21@gmail.com>
2022-04-27 18:22:49 +0530

    Routing- Consuming Route Parameters

--- src/components/RoutingApp/ProductDetails.jsx
+++ src/components/RoutingApp/ProductDetails.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function ProductDetails() {
+export default function ProductDetails(props) {
+    const productId = props.match.params.id
     return (
         <div>
-            <h1>Product Details - </h1>
-            <button>Save</button>
+            <h1>Product Details - {productId}</h1>
+            <button className='btn btn-success btn-sm m-2'>Save</button>
         </div>
     )
 }
