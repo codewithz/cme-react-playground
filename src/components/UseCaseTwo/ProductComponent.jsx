f9a3817a91e42d5a698d16b09f9a5b5b8c0f893f
Code With Z <codewithz21@gmail.com>
2022-04-27 15:26:12 +0530

    Parent to Child Communication

--- src/components/UseCaseTwo/ProductComponent.jsx
+++ src/components/UseCaseTwo/ProductComponent.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function ProductComponent() {
+export default function ProductComponent(props) {
+
+    const [name, setName] = useState(props.name)
+    const [quantity, setQuantity] = useState(props.quantity)
+    const [productId, setProductId] = useState(props.id)
+
+
+
+    const styles = {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+
+    const formatQuantity = () => quantity === 0 ? 'Zero' : quantity
+
+    const handleIncrement = () => {
+        setQuantity(quantity + 1)
+    }
+
+    const handleDecrement = () => {
+        if (quantity >= 1) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    const getBadgeClasses = () => {
+        let classes = "badge m-2 badge-"
+        classes += (quantity === 0) ? "warning" : "primary"
+        return classes
+    }
     return (
         <div>
-            <h4>Product Name</h4>
-            <button className='btn btn-secondary btn-sm m-2'>
+            <h4>{name}</h4>
+            <button className='btn btn-secondary btn-sm m-2'
+                onClick={handleIncrement}
+            >
                 +
             </button>
-            <span className="badge m-2 badge-warning">2</span>
-            <button className='btn btn-info btn-sm m-2'>
+            <span style={styles} className={getBadgeClasses()}>{formatQuantity()}</span>
+            <button className='btn btn-info btn-sm m-2'
+                onClick={handleDecrement}
+            >
                 -
             </button>
         </div >
--- src/components/UseCaseTwo/ProductListComponent.jsx
+++ src/components/UseCaseTwo/ProductListComponent.jsx
@@ -4,12 +4,12 @@ import ProductComponent from './ProductComponent'
 export default function ProductListComponent() {
 
     const productList = [
-        { id: 1, name: 'Bread', quanity: 3 },
-        { id: 2, name: 'Butter', quanity: 1 },
-        { id: 3, name: 'Jam', quanity: 1 },
-        { id: 4, name: 'Maggi', quanity: 4 },
-        { id: 5, name: 'Eggs', quanity: 6 },
-        { id: 6, name: 'Salt', quanity: 1 }
+        { id: 1, name: 'Bread', quantity: 3 },
+        { id: 2, name: 'Butter', quantity: 1 },
+        { id: 3, name: 'Jam', quantity: 0 },
+        { id: 4, name: 'Maggi', quantity: 4 },
+        { id: 5, name: 'Eggs', quantity: 6 },
+        { id: 6, name: 'Salt', quantity: 1 }
     ]
 
     const [products, setProducts] = useState(productList)
@@ -18,9 +18,15 @@ export default function ProductListComponent() {
 
     return (
         <div>
+
             {
                 products.map((product) =>
-                    <ProductComponent />
+                    <ProductComponent
+                        key={product.id}
+                        name={product.name}
+                        quantity={product.quantity}
+                        id={product.id}
+                    />
                 )
             }
             <br />
