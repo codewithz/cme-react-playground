6cfb5381ae81b5f894963e357fd6b1e0c6009f48
Code With Z <codewithz21@gmail.com>
2022-04-27 17:15:40 +0530

    Routing - Skeleton Files

---
+++ src/components/RoutingApp/admin/AdminPosts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminPosts() {
+    return (
+        <div>
+            <h1>AdminPosts</h1>
+        </div>
+    )
+}
---
+++ src/components/RoutingApp/admin/Users.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Users() {
+    return (
+        <div><h1>Admin Users</h1></div>
+    )
+}
