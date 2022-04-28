5f5340c9e1de98e1eb8011f2e3a3ff24b444747c
Code With Z <codewithz21@gmail.com>
2022-04-28 17:26:05 +0530

    Http Calls: Post Page UI Designed

--- src/components/HttpExample/PostComponent.jsx
+++ src/components/HttpExample/PostComponent.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PostComponent() {
+
+    const postsList = [
+        { id: 1, title: "Title something" },
+        { id: 2, title: "Title something" },
+        { id: 3, title: "Title something" },
+        { id: 4, title: "Title something" },
+    ]
+
+    const [posts, setPosts] = useState(postsList)
+
     return (
-        <div>PostComponent</div>
+        <div>
+            <button className='btn btn-primary' 
+            style={{ marginBottom: 20 }}>
+                Add
+            </button>
+
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        posts.map((post)=>(
+                            <tr key={post.id}>
+                                <td>{post.title}</td>
+                                <td>
+                                    <button className='btn btn-sm btn-warning m-2'>
+                                        Update
+                                    </button>
+                                </td>
+                                <td>
+                                    <button className='btn btn-sm btn-danger m-2'>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
