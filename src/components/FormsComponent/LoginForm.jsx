bafa3db4deaa0c52328a99462e9a9e7bdd2cb530
Code With Z <codewithz21@gmail.com>
2022-04-29 16:52:09 +0530

    Forms - Controlled Elements - Multiple Events

--- src/components/FormsComponent/LoginForm.jsx
+++ src/components/FormsComponent/LoginForm.jsx
@@ -5,10 +5,17 @@ export default function LoginForm() {
 
     const handleChange = (event) => {
         console.log("Event:", event)
+        const { name, value } = event.currentTarget
         const accountClone = { ...account }
-        accountClone.username = event.currentTarget.value
+        accountClone[name] = value
         setAccount(accountClone)
     }
+    // const handleChangePassword = (event) => {
+    //     console.log("Event:", event)
+    //     const accountClone = { ...account }
+    //     accountClone.password = event.currentTarget.value
+    //     setAccount(accountClone)
+    // }
 
     return (
         <div>
@@ -20,12 +27,19 @@ export default function LoginForm() {
                         type="text"
                         className='form-control'
                         id='username'
+                        name='username'
                         onChange={handleChange}
                     />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className='form-control' id='password' />
+                    <input
+                        type="password"
+                        className='form-control'
+                        id='password'
+                        name='password'
+                        onChange={handleChange}
+                    />
                 </div>
                 <button className="btn btn-warning btn-sm m-2">Login</button>
             </form>
