d9954270fe4aabb77a2617483f8e69ab6469c0b4
Code With Z <codewithz21@gmail.com>
2022-04-29 16:42:19 +0530

    Forms: Controlled Elements - Specific

--- src/components/FormsComponent/LoginForm.jsx
+++ src/components/FormsComponent/LoginForm.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginForm() {
+    const [account, setAccount] = useState({ username: 'codewithz', password: '' })
+
+    const handleChange = (event) => {
+        console.log("Event:", event)
+        const accountClone = { ...account }
+        accountClone.username = event.currentTarget.value
+        setAccount(accountClone)
+    }
+
     return (
         <div>
             <h1>LoginForm</h1>
             <form>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" className='form-control' id='username' />
+                    <input
+                        type="text"
+                        className='form-control'
+                        id='username'
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
@@ -15,6 +29,9 @@ export default function LoginForm() {
                 </div>
                 <button className="btn btn-warning btn-sm m-2">Login</button>
             </form>
+
+            <p>Username:{account.username}</p>
+            <p>Password:{account.password}</p>
         </div>
     )
 }
